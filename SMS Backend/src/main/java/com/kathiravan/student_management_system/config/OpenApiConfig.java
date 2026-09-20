cd82052bdee0f54af58c7257329e468a13aa16e9
Kathiravan G <kathiravan.g.2005@gmail.com>
2026-09-20 21:54:37 +0530

    Fix Swagger HTTPS URL

--- SMS Backend/src/main/java/com/kathiravan/student_management_system/config/OpenApiConfig.java
+++ SMS Backend/src/main/java/com/kathiravan/student_management_system/config/OpenApiConfig.java
@@ -2,8 +2,13 @@ package com.kathiravan.student_management_system.config;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @SecurityScheme(
@@ -12,4 +17,15 @@ import org.springframework.context.annotation.Configuration;
         scheme = "bearer",
         bearerFormat = "JWT")
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        Server server = new Server();
+        server.setUrl(
+                "https://distinguished-analysis-production-145f.up.railway.app");
+
+        return new OpenAPI()
+                .servers(List.of(server));
+    }
 }
