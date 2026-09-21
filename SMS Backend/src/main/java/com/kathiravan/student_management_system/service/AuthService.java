6f2da89a8abd749fe8497010dde9cd1f07e4932b
Kathiravan G <kathiravan.g.2005@gmail.com>
2026-09-21 08:32:45 +0530

    Fixed AuthService login method

--- SMS Backend/src/main/java/com/kathiravan/student_management_system/service/AuthService.java
+++ SMS Backend/src/main/java/com/kathiravan/student_management_system/service/AuthService.java
@@ -4,10 +4,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.kathiravan.student_management_system.dto.LoginRequest;
+import com.kathiravan.student_management_system.dto.LoginResponse;
 import com.kathiravan.student_management_system.dto.RegisterRequest;
 import com.kathiravan.student_management_system.entity.User;
 import com.kathiravan.student_management_system.repository.UserRepository;
-import com.kathiravan.student_management_system.dto.LoginResponse;
 
 @Service
 public class AuthService {
@@ -17,31 +17,53 @@ public class AuthService {
     private final JwtService jwtService;
 
     public AuthService(UserRepository userRepository,
-                   PasswordEncoder passwordEncoder,
-                   JwtService jwtService) {
+                       PasswordEncoder passwordEncoder,
+                       JwtService jwtService) {
 
-    this.userRepository = userRepository;
-    this.passwordEncoder = passwordEncoder;
-    this.jwtService = jwtService;
-}
-public String register(RegisterRequest request) {
-
-    System.out.println("Register API Called");
-    System.out.println("Username = " + request.getUsername());
-
-    if (userRepository.findByUsername(request.getUsername()).isPresent()) {
-        return "Username already exists";
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
     }
 
-    User user = new User();
-    user.setUsername(request.getUsername());
-    user.setPassword(passwordEncoder.encode(request.getPassword()));
-    user.setRole(request.getRole());
+    public String register(RegisterRequest request) {
 
-    User savedUser = userRepository.save(user);
+        System.out.println("Register API Called");
+        System.out.println("Username = " + request.getUsername());
 
-    System.out.println("Saved User ID = " + savedUser.getId());
+        if (userRepository.findByUsername(request.getUsername()).isPresent()) {
+            return "Username already exists";
+        }
 
-    return "User Registered Successfully";
-}
+        User user = new User();
+
+        user.setUsername(request.getUsername());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRole(request.getRole());
+
+        User savedUser = userRepository.save(user);
+
+        System.out.println("Saved User ID = " + savedUser.getId());
+
+        return "User Registered Successfully";
+    }
+
+    public LoginResponse login(LoginRequest request) {
+
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() ->
+                        new RuntimeException("Invalid Username"));
+
+        if (!passwordEncoder.matches(
+                request.getPassword(),
+                user.getPassword())) {
+
+            throw new RuntimeException("Invalid Password");
+        }
+
+        String token = jwtService.generateToken(
+                user.getUsername(),
+                user.getRole());
+
+        return new LoginResponse(token);
+    }
 }
